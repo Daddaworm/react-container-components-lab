7e8825b6143386faeae008b58ea06945ab395636
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-04 16:31:35 -0400

    commit tree 04f20601248d51f6b8446e60f3d4d7fd852ea979

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,29 @@
 // Code MovieReviews Here
+import React, { Component } from 'react'
+import LatestMovieReviewsContainer from './LatestMovieReviewsContainer'
+import SearchableMovieReviewsContainer from './LatestMovieReviewsContainer'
+
+
+
+
+function MovieReviews(props) {
+    const reviews = props.reviews.map(review => {
+        return (
+            <div className='review' key={review.display_title}>
+                <h2>Headline: {review.headline}</h2>
+                {/* <img src={review.multimedia.src}/> */}
+                <p>{review.summary_short}</p>
+                <p>MPAA Rating: {review.mpaa_rating}</p>
+                <h4>Written by: {review.byline}</h4>
+            </div>
+        )
+        
+    })
+        return(
+            <div className='review-list' >{reviews}</div>
+    )
+}
+MovieReviews.defaultProps = {
+    reviews: []
+}
+export default MovieReviews
